605e254294f40d18d4dd9f2be66bb45405ad21b9
lizzyjoo <jejviolinist@gmail.com>
2025-03-13 11:34:44 -0400

    include express validation

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-
+const { body, validationResult } = require("express-validator");
 const indexRouter = Router();
 
 // sample messages
@@ -28,18 +28,63 @@ indexRouter.get("/new", (req, res) => {
   res.render("form", { title: "Leave a message!" });
 });
 
-indexRouter.post("/new", (req, res) => {
-  // get contents from the form
-  const messageUser = req.body.messageUser;
-  const messageText = req.body.messageText;
-  messages.push({
-    text: messageText,
-    user: messageUser,
-    added: new Date(),
-    likes: 0,
-  });
-  res.redirect("/");
-});
+// validation middleware
+indexRouter.get(
+  "/new",
+  [
+    body("messageUser")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Name cannot be empty")
+      .isLength({ max: 50 })
+      .withMessage("Name cannot exceed 50 characters")
+      .escape(),
+
+    body("messageText")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("Message cannot be empty")
+      .isLength({ max: 500 })
+      .withMessage("Message cannot exceed 500 characters")
+      .escape(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // If there are errors, re-render the form with error messages
+      return res.render("form", {
+        title: "Add a new message!",
+        errors: errors.array(),
+      });
+    }
+
+    // If validation passes, add the message
+    const messageUser = req.body.messageUser;
+    const messageText = req.body.messageText;
+
+    messages.push({
+      text: messageText,
+      user: messageUser,
+      added: new Date(),
+      likes: 0,
+    });
+
+    res.redirect("/");
+  }
+);
+// indexRouter.post("/new", (req, res) => {
+//   // get contents from the form
+//   const messageUser = req.body.messageUser;
+//   const messageText = req.body.messageText;
+//   messages.push({
+//     text: messageText,
+//     user: messageUser,
+//     added: new Date(),
+//     likes: 0,
+//   });
+//   res.redirect("/");
+// });
 
 indexRouter.post("/like/:messageIndex", (req, res) => {
   const messageIndex = parseInt(req.params.messageIndex);
