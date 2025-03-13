f07b6bebd51ed7b15ef519cc4de8540b7594d511
lizzyjoo <jejviolinist@gmail.com>
2025-03-13 01:32:59 -0400

    add fake music player

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -8,11 +8,13 @@ const messages = [
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
+    likes: 0,
   },
   {
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
+    likes: 0,
   },
 ];
 
@@ -23,14 +25,45 @@ indexRouter.get("/", (req, res) => {
 
 // New Message form route
 indexRouter.get("/new", (req, res) => {
-  res.render("form", { title: "Add a new message!" });
+  res.render("form", { title: "Leave a message!" });
 });
 
 indexRouter.post("/new", (req, res) => {
   // get contents from the form
   const messageUser = req.body.messageUser;
   const messageText = req.body.messageText;
-  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  messages.push({
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+    likes: 0,
+  });
   res.redirect("/");
+});
+
+indexRouter.post("/like/:messageIndex", (req, res) => {
+  const messageIndex = parseInt(req.params.messageIndex);
+  // Validate messageIndex
+  if (
+    isNaN(messageIndex) ||
+    messageIndex < 0 ||
+    messageIndex >= messages.length
+  ) {
+    return res.status(400).json({ success: false });
+  }
+
+  // Initialize likes if it doesn't exist
+  if (typeof messages[messageIndex].likes !== "number") {
+    messages[messageIndex].likes = 0;
+  }
+
+  // Increment likes
+  messages[messageIndex].likes++;
+
+  // Return new count
+  res.json({
+    success: true,
+    likes: messages[messageIndex].likes,
+  });
 });
 module.exports = indexRouter;
